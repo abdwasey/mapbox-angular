4b5efb254ef1af02e5b91603d1a7b7e487911f51
Wasey <abdul.wasey17@outlook.com>
2021-04-25 14:07:55 +0500

    Add GeoJson Line

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import * as mapboxgl from 'mapbox-gl';
 
-
-export class locationTask{
+export class locationTask {
   lat: any;
   lng: any;
 }
@@ -17,10 +16,11 @@ export class AppComponent implements OnInit {
 
   lat = 24.8607;
   lng = 67.0011;
-  wasey = true;
   markerCoordinates: locationTask;
 
+
   ngOnInit() {
+
     var _popup = new mapboxgl.Marker(map);
     mapboxgl.accessToken = environment.mapBoxToken;
     var map = new mapboxgl.Map({
@@ -33,9 +33,41 @@ export class AppComponent implements OnInit {
     map.on('style.load', function () {
       map.on('click', function (e) {
         var coordinates = e.lngLat;
+        console.log(coordinates);
         this.markerCoordinates = coordinates;
         _popup.setLngLat(this.markerCoordinates)
         _popup.addTo(map);
+      });
+    });
+
+    map.on('load', function () {
+      map.addSource('route', {
+        'type': 'geojson',
+        'data': {
+          'type': 'Feature',
+          'properties': {},
+          'geometry': {
+            'type': 'LineString',
+            'coordinates': [[67.00163375959374, 24.860797346189486],
+            [67.0015425644873, 24.860142691597318],
+            [67.0008344613075, 24.860909294210984],
+            [67.00113588378778, 24.863345146576066]
+            ]
+          }
+        }
+      });
+      map.addLayer({
+        'id': 'route',
+        'type': 'line',
+        'source': 'route',
+        'layout': {
+          'line-join': 'round',
+          'line-cap': 'round'
+        },
+        'paint': {
+          'line-color': 'green',
+          'line-width': 8
+        }
       });
     });
   }
